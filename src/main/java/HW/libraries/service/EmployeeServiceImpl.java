f4809313e9c28b9cfb6a3a56732ad5ca18051375
IDFCSGFY <idfcsgfy@gmail.com>
2024-10-22 22:51:16 +0700

    hw done; theme: libraries

--- src/main/java/HW/libraries/service/EmployeeServiceImpl.java
+++ src/main/java/HW/libraries/service/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import HW.libraries.exception.EmployeeAlreadyAddedException;
 import HW.libraries.exception.EmployeeNotFoundException;
 import HW.libraries.exception.EmployeeStorageIsFullException;
 import HW.libraries.model.Employee;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -14,6 +15,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private final Map<String, Employee> employees;
     private final int MAX_EMPLOYEE_COUNT = 1000;
+    private final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
     public EmployeeServiceImpl() {
         this.employees = new HashMap<>();
@@ -23,9 +25,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         if (employees.size() >= MAX_EMPLOYEE_COUNT) {
             throw new EmployeeStorageIsFullException();
         }
-        if (firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty()) {
+        if (StringUtils.isEmpty(firstName) || StringUtils.isEmpty(lastName)
+                || !StringUtils.containsOnly(firstName, LETTERS) || !StringUtils.containsOnly(lastName, LETTERS)) {
             throw new BadParamException();
         }
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
         Employee target = new Employee(firstName, lastName);
         String tempKey = String.join("_", firstName, lastName);
         if (employees.containsKey(tempKey)) {
@@ -39,9 +44,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         if (employees.size() >= MAX_EMPLOYEE_COUNT) {
             throw new EmployeeStorageIsFullException();
         }
-        if (firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty()) {
+        if (StringUtils.isEmpty(firstName) || StringUtils.isEmpty(lastName)
+                || !StringUtils.containsOnly(firstName, LETTERS) || !StringUtils.containsOnly(lastName, LETTERS)) {
             throw new BadParamException();
         }
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
         Employee target = new Employee(firstName, lastName, dID, wage);
         String tempKey = String.join("_", firstName, lastName);
         if (employees.containsKey(tempKey)) {
@@ -52,6 +60,12 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     public Employee removeEmployee(String firstName, String lastName) {
+        if (StringUtils.isEmpty(firstName) || StringUtils.isEmpty(lastName)
+                || !StringUtils.containsOnly(firstName, LETTERS) || !StringUtils.containsOnly(lastName, LETTERS)) {
+            throw new BadParamException();
+        }
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
         Employee target = new Employee(firstName, lastName);
         String tempKey = String.join("_", firstName, lastName);
         if (!employees.containsKey(tempKey)) {
@@ -62,7 +76,12 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     public Employee findEmployee(String firstName, String lastName) {
-        Employee target = new Employee(firstName, lastName);
+        if (StringUtils.isEmpty(firstName) || StringUtils.isEmpty(lastName)
+                || !StringUtils.containsOnly(firstName, LETTERS) || !StringUtils.containsOnly(lastName, LETTERS)) {
+            throw new BadParamException();
+        }
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
         String tempKey = String.join("_", firstName, lastName);
         if (!employees.containsKey(tempKey)) {
             throw new EmployeeNotFoundException();
